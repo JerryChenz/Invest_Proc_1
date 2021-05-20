45ff53195ba4513e9d15d05ff9c1347ae88da436
Jerry <jerrychen.works@outlook.com>
2021-05-20 10:59:38 +0800

    added notion token

--- src/main.py
+++ src/main.py
@@ -4,14 +4,16 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def notion_price():
     import yfinance as yf
     from notion.client import NotionClient
     from datetime import datetime
 
-    client = NotionClient(token_v2="your token key")
+    client = NotionClient(token_v2="514b02c7f5ac3c25e23915fb3e5e7a468ae3d881c2eeaa03bd1d87e788b062cfb5fb2ad0933a10e144095a551c2e1001eae35a90e1e01e8c8db4606f7b7a92c96e6a4b1b8572bf4b6a0bd60dbc4d")
 
-    cv = client.get_collection_view("https://www.notion.so/fadfdsf32321514?v=1223r234jhkhkh12")
+    cv = client.get_collection_view("https://www.notion.so/Valuation-Project-8075400fc5614af0a5ed7a94fa8e984e")
+
+    print("The title is:", page.title)
 
     tickers = ['AAPL', 'MSFT', 'VWRL.AS', 'CHF=X', 'AMZN', 'INTC']
     for ticker in tickers:
@@ -30,6 +32,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    notion_price()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
