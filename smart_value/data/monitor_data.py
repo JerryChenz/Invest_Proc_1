6cb16429b2133ea53507155750d20afa3d8f8479
jerry <jerrychen.works@gmail.com>
2025-10-06 12:32:29 +0800

    Updated Models and Monitors

--- smart_value/data/monitor_data.py
+++ smart_value/data/monitor_data.py
@@ -12,13 +12,15 @@ market_yield_pos = {
 portfolio_mgmt_pos = {
     "benchmark_return": "$C$10",
     "cash_yield": "$C$11",
-    "max_holdings": "C27",
-    "single_investment_cap": "C28",
-    "correct_chance": "C32", "incorrect_loss": "E32",
-    "target_return": "C33",
-    "entry_yield": "C35",
-    "holding_period": "C37", "equity_cost": "E37",
-    "min_cash_reserve": "E66"
+    "equity_cost": "$C$12",
+    "max_holdings": "C30",
+    "single_investment_cap": "C31",
+    "holding_period": "C36",
+    "correct_chance": "C37",
+    "incorrect_loss": "C38",
+    "target_return": "C46",
+    "entry_yield": "C47",
+    "min_cash_reserve": "E73"
 }
 
 opportunities_headers = {
